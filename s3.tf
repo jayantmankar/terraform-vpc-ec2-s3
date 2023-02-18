326cf19d9ce4b0c97ee71e1f44834141d32119e5
jayant <mankar.jay@gmail.com>
2023-02-18 18:21:30 +0530

    terraform jenkin file

--- s3.tf
+++ s3.tf
@@ -1,13 +1,11 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket" "b" {
+ # bucket = "my-tf-test-bucket"
+  #tags = {
+   # Name        = "My bucket"
+    #Environment = "Dev"
+  #}
+#}
+#resource "aws_s3_bucket_acl" "example" {
+ # bucket = aws_s3_bucket.b.id
+  #acl    = "private"
+#}
