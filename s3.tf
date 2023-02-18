e6eb2f81677c536d2544756393aa7175853884d7
jayant <mankar.jay@gmail.com>
2023-02-18 18:01:02 +0530

    terraform jenkin file

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "Jayant" {
   bucket = "terraform-sample-s3bucket00111"
-  acl    = "public-read"
+  aws_s3_bucket_acl    = "public-read"
 
   tags = {
     Name        = "awsclass-aug2022"
